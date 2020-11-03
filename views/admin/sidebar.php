9a8bd9ebac3fda2422c7ee17eac215fe4c7ce4cb
huynguyeexn <huynguyeexn@gmail.com>
2020-11-03 23:45:49 +0700

    cắt HTML trang ADMIN INDEX

---
+++ views/admin/sidebar.php
@@ -0,0 +1,45 @@
+<aside class="sidebar-left border-right bg-white shadow" id="leftSidebar" data-simplebar>
+    <a href="#" class="btn collapseSidebar toggle-btn d-lg-none text-muted ml-2 mt-3" data-toggle="toggle">
+        <i class="fe fe-x"><span class="sr-only"></span></i>
+    </a>
+    <nav class="vertnav navbar navbar-light">
+        <!-- nav bar -->
+        <div class="w-100 mb-4 d-flex">
+            <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="./index.html">
+                Thàn nào rảnh thêm logo vô dùm với
+            </a>
+        </div>
+        <ul class="navbar-nav flex-fill w-100 mb-2">
+            <li class="nav-item dropdown">
+                <a href="#dashboard" aria-expanded="false" class="nav-link">
+                    <i class="fe fe-home fe-16"></i>
+                    <span class="ml-3 item-text">Trang chủ</span>
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#dashboard" aria-expanded="false" class="nav-link">
+                    <i class="fe fe-shopping-cart" aria-hidden="true"></i>
+                    <span class="ml-3 item-text">Đơn hàng</span>
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#dashboard" aria-expanded="false" class="nav-link">
+                    <i class="fe fe-box fe-16"></i>
+                    <span class="ml-3 item-text">Sản phẩm</span>
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#dashboard" aria-expanded="false" class="nav-link">
+                    <!-- <i class="fe fe-home fe-16"></i> -->
+                    <span class="ml-3 item-text">...</span>
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#dashboard" aria-expanded="false" class="nav-link">
+                    <!-- <i class="fe fe-home fe-16"></i> -->
+                    <span class="ml-3 item-text">...</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</aside>
