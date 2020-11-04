1a11a40082060b17c64a5f72628c14f63e8288ac
vuongdien <dienlnvps13848@fpt.edu.vn>
2020-11-04 13:56:37 +0700

    Thay doi port

---
+++ views/products/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<head>
+    <?php include_once('views/layouts/meta.php'); ?>
+</head>
+</head>
+<body>
+    <?php
+        require_once("views/templates/Header.php");
+    ?>
+    <body>
