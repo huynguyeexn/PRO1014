eeafc1f2543c9e1425b7b49e767bc6ac185649e7
dungle21092001 <dungle21092001@gmail.com>
2020-11-17 14:51:27 +0700

    Việt hoá trang shop

--- controllers/ShopController.php
+++ controllers/ShopController.php
@@ -74,13 +74,8 @@
                     if(in_array($name,array_column($_SESSION['filter'],'name'))){
                         foreach($_SESSION['filter'] as $key => $v){
                             if($v['name'] == $name){
-                                if($name == 'tag'){
                                     $_SESSION['filter'][$key]['name'] = $name;
                                     $_SESSION['filter'][$key]['value'] = $values;
-                                }else{
-                                    $_SESSION['filter'][$key]['name'] = $name;
-                                    $_SESSION['filter'][$key]['value'] = $values;
-                                }
                             }
                         }
                     }else{
@@ -88,9 +83,15 @@
                     }
                 }
             }else if(isset($_GET['start'])){
-                $offset = $_GET['start'];
-                $limit = 6;
-                $value = ceil($offset/6+1);
+                if(!empty($_SESSION['show'])){
+                    $show = $_SESSION['show']['value'];
+                }else{
+                    $show = 6;
+                }
+                $x = $_GET['start'];
+                $offset = ($x*$show) - $show;
+                $limit = $show;
+                $value = ceil($offset/$show+1);
                 $name = 'page';
                 $_SESSION['page'] = array('name'=>$name,'value'=>$value);
             }else if(isset($_GET['sort'])){
@@ -103,6 +104,44 @@
                 $value = $lower.'-'.$upper;
                 $name = 'price';
                 $_SESSION['price'] = array('name'=>$name,'value'=>$value,'lower'=>$lower,'upper'=>$upper);
+            }else if(isset($_GET['show'])){
+                $value = $_GET['show'];
+                $name = 'show';
+                $_SESSION['show'] = array('name'=>$name,'value'=>$value);
+            }else if(isset($_GET['button'])){
+                $value = $_GET['button'];
+                if(!empty($_SESSION['show'])){
+                    $show = $_SESSION['show']['value'];
+                }else{
+                    $show = 6;
+                }
+                if(!empty($_SESSION['page'])){
+                    if($value == 'next'){
+                        $name = $_SESSION['page']['name'];
+                        $x = $_SESSION['page']['value'] + 1;
+                        $offset = ($x*$show) - $show;
+                        $limit = $show;
+                    }else{
+                        $name = $_SESSION['page']['name'];
+                        $x = $_SESSION['page']['value'] - 1;
+                        $offset = ($x*$show) - $show;
+                        $limit = $show;
+                    }
+                    $_SESSION['page'] = array('name'=>$name,'value'=>$x);
+                }else{
+                    if($value == 'next'){
+                        $name = 'page';
+                        $x = 1 + 1;
+                        $offset = ($x*$show) - $show;
+                        $limit = $show;
+                    }else{
+                        $name = 'page';
+                        $x = 1 - 1;
+                        $offset = ($x*$show) - $show;
+                        $limit = $show;
+                    }
+                    $_SESSION['page'] = array('name'=>$name,'value'=>$x);
+                }
             }
 
             $where ='';
@@ -129,6 +168,12 @@
                     }
                 }
             }
+            if(!empty($_SESSION['price'])){
+                $true = 2;
+                $lower = $_SESSION['price']['lower'];
+                $upper = $_SESSION['price']['upper'];
+                $where .= !empty($where) ? ' and price BETWEEN '.$lower.' and '.$upper.'': ' where price BETWEEN '.$lower.' and '.$upper.'';
+            }
             if(!empty($_SESSION['sort'])){
                 $true = 2;
                 $value = $_SESSION['sort']['value']; 
@@ -143,31 +188,30 @@
                     $where .= ' order by price desc';
                 }
             }
-            if(!empty($_SESSION['price'])){
-                $true = 2;
-                $lower = $_SESSION['price']['lower'];
-                $upper = $_SESSION['price']['upper'];
-                $where .= !empty($where) ? ' and price BETWEEN '.$lower.' and '.$upper.'': ' where price BETWEEN '.$lower.' and '.$upper.'';
-            }
-            if(isset($_GET['start'])){
+            if(isset($_GET['start']) || isset($_GET['button'])){
                 if($true == 2){
                     $where .= ' limit '.$limit.' offset '.$offset.'';
-                    $page = getProductByFilter($where); 
+                    $pr = getProductByFilter($where); 
                 }else{
-                    $page = getProductByOffset($limit, $offset);
+                    $pr = getProductByOffset($limit, $offset);
                 }
             }else{
-                $page = getProductByFilter($where);
+                $pr = getProductByFilter($where);
             }
             
             
             $lan = 0;
             $luot =0;
+            if(!empty($_SESSION['show'])){
+               $show = $_SESSION['show']['value']; 
+            }else{
+                $show = 6;
+            }
             $sp = '';
-            foreach($page as $p){  
+            foreach($pr as $p){  
                 $luot++;
                 $lan++;
-                if($luot < 7){
+                if($luot <= $show){
                     $sp .= '
                         <div class="col-lg-4 col-md-6">
                             <div class="boxa single-product">
@@ -204,33 +248,56 @@
             } 
             $myArr = array(array($sp));
             $number = 0;
-            $lan = $lan / 6;
+            $lan = $lan / $show;
             $page = '';
             if(ceil($lan) < 4){
                 if(ceil($lan) == 0){
                     $page = '';
                 }else{
-                    $page = '<a href="#" class="prev-arrow"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></a>';
+                    if(!empty($_SESSION['page'])){
+                        if($_SESSION['page']['value'] == 2){
+                            $page ='';
+                        }else{
+                            $page .= '<a href="#" class="prev-arrow"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></a>';
+                        }
+                    }
                     for ($i=0; $i < ceil($lan); $i++){ 
                         $number++;
                         $page .= '<a onclick="page('.$number.');">'.$number.'</a>';
                     }
-                    $page .= '<a href="#" class="next-arrow"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>';
+                    if(!empty($_SESSION['page'])){
+                        if($_SESSION['page']['value'] == $lan){
+                            $page .='';
+                        }else{
+                            $page .= '<a href="#" class="next-arrow"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>';
+                        }
+                    }
                 }
-            }else{
-                for ($i=0; $i < 4; $i++){ 
-                    $number++;
-                    $page .= '
-                        <a href="#" class="prev-arrow"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></a>	
-                        <a onclick="page('.$number.');">'.$number.'</a>
-                    ';
-                }
-                $page .= '
-                    <a href="#" class="dot-dot"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a>
-                    <a href="#">'.ceil($lan).'</a>
-                    <a href="#" class="next-arrow"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
-                ';
             }
+            // else{
+            //     for ($i=0; $i < 4; $i++){ 
+            //         $number++;
+            //         if($number == 1){
+            //             $page .= '<a onclick="page('.$number.');">'.$number.'</a>';
+            //             $page .= '<a href="#" class="next-arrow"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>';
+            //        }else if($number == $lan){
+            //            $page = '<a href="#" class="prev-arrow"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></a>';
+            //            $page .= '<a onclick="page('.$number.');">'.$number.'</a>';
+            //         }else{
+            //            $page = '<a href="#" class="prev-arrow"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></a>';
+            //            $page .= '<a onclick="page('.$number.');">'.$number.'</a>';
+            //         }
+            //         $page .= '
+            //             <a href="#" class="prev-arrow"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></a>	
+            //             <a onclick="page('.$number.');">'.$number.'</a>
+            //         ';
+            //     }
+            //     $page .= '
+            //         <a href="#" class="dot-dot"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a>
+            //         <a href="#">'.ceil($lan).'</a>
+            //         <a href="#" class="next-arrow"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+            //     ';
+            // }
             array_push($myArr,array($page));
             if(isset($_SESSION['filter'])){
                 foreach($_SESSION['filter'] as $filter => $value){
@@ -240,13 +307,17 @@
             if(isset($_SESSION['sort'])){
                 array_push($myArr,array($_SESSION['sort']['value'],$_SESSION['sort']['name']));
             }
-            if(isset($_SESSION['page'])){
+            if(!empty($_SESSION['page'])){
                 array_push($myArr,array($_SESSION['page']['value'],$_SESSION['page']['name']));
             }
             if(isset($_SESSION['price'])){
                 array_push($myArr,array($_SESSION['price']['value'],$_SESSION['price']['name']));
             }
+            if(isset($_SESSION['show'])){
+                array_push($myArr,array($_SESSION['show']['value'],$_SESSION['show']['name']));
+            }
             echo json_encode($myArr);
+            // print_r ($pr);
             // echo $where;
             break;
 
