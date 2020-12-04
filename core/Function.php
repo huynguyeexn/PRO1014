907db0eb8fe79f52701c4416ce882d4ca87352cd
huynguyeexn <huynguyeexn@gmail.com>
2020-12-04 09:51:13 +0700

    Format lại connect.php, function.php

--- core/Function.php
+++ core/Function.php
@@ -1,8 +1,7 @@
 <?php
 /*
-Dùng để viết các function dùng chung cho cả project
+Dùng để viết các hàm tiện ích dùng chung cho cả project
 */
-
 
 // Định dạng từ số thành tiền.
 function numToMoney($number){
@@ -10,20 +9,24 @@ function numToMoney($number){
     $formatter = new NumberFormatter( 'vi_VI', NumberFormatter::CURRENCY);
     return $formatter->formatCurrency($number, "VND")."\n";
 }
-
-
 // Trả về thời gian hiện tại.
 function now(){
     date_default_timezone_set("Asia/Ho_Chi_Minh");
-    $date = new DateTime(date("Y-m-d H:i:s"), new DateTimeZone('Asia/Ho_Chi_Minh'));
+    $date = new DateTime(
+        date("Y-m-d H:i:s"),
+        new DateTimeZone('Asia/Ho_Chi_Minh')
+    );
     return $date->format('Y-m-d H:i:s');
 }
 
+// Nút thêm giỏ hàng, lưu vào session.
 function addToCart($productID, $size){
     if (isset($_SESSION['cart'])){
-        $search_items = array('id'=>$productID, 'size'=>$size); 
+        $search_items = array(
+            'id'=>$productID,
+            'size'=>$size
+        ); 
         $result = searchMultiKey($_SESSION['cart'], $search_items); 
-        
         if($result){
             $_SESSION['cart'][$result[0]['index']]['quantity'] += 1;
         }else{
@@ -50,35 +53,18 @@ function addToCart($productID, $size){
     return $_SESSION['cart'];
 }
 
+// Hàm tìm kiếm các key trong array
 function searchMultiKey($array, $search_list) { 
-  
-    // Create the result array 
     $result = array(); 
-  
-    // Iterate over each array element 
     foreach ($array as $key => $value) { 
-  
-        // Iterate over each search condition 
         foreach ($search_list as $k => $v) { 
-      
-            // If the array element does not meet 
-            // the search condition then continue 
-            // to the next element 
             if (!isset($value[$k]) || $value[$k] != $v) 
             { 
-                  
-                // Skip two loops 
                 continue 2; 
             } 
         } 
-      
-        // Append array element's key to the 
-        //result array 
-
         $value += ['index' => $key];
         $result[] = $value; 
-    } 
-  
-    // Return result  
+    }
     return $result; 
 } 
